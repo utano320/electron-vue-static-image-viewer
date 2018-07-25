cf7ed1eb5e2ab822e2da1840ed01ddf8eb331e40
Mitsuo Utano <utano320@gmail.com>
2018-07-25 22:09:00 +0900

    electron-vueテンプレート

---
+++ src/renderer/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <landing-page></landing-page>
+  </div>
+</template>
+
+<script>
+import LandingPage from "@/components/LandingPage";
+
+export default {
+  name: "static-image-viewer",
+  components: {
+    LandingPage
+  }
+};
+</script>
+
+<style>
+/* CSS */
+</style>
